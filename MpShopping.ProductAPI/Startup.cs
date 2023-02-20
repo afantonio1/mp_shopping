5f12f3c7b279c7049709a838fc001c0745754541
Alessandro <afantonio1@gmail.com>
2023-02-20 19:46:03 -0300

    US - Adicionando o projeto ao repositorio.

---
+++ MpShopping.ProductAPI/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MpShopping.ProductAPI.Model.Context;
+
+namespace MpShopping.ProductAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["MySQlConnection:MySQlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options
+                    .UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 32))));
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
